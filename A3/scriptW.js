3e1efa9c7c0a5e85bd72d3a39d545104ab30983c
LenaMarie1903 <lena.m.fleig@gmail.com>
2023-12-12 16:26:11 +0100

    Poition und Größe sind random

--- A3/scriptW.js
+++ A3/scriptW.js
@@ -1,7 +1,12 @@
-let Chrismas = ["assets/christmastree.png"];
+//let Chrismas = ["assets/christmastree.png"];
 let stars = ["assets/Objekt2.png", , "assets/Stern.png", "assets/stern1.png", "assets/stern2.png", "assets/stern3.png", "assets/stern4.png"];
 /* Ein leeres Array erstellen*/
 let imageArray = [];
+
+//Hier wird das Grundgerüst festgelegt um random Größe und Position anzuordnen. 
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 function loadImages() {
     /* Der a-marker aus HTML wird ausgewählt durch den querySelector und in der Variable var marker reingeschreiben*/
@@ -14,14 +19,24 @@ function loadImages() {
         /* wenn das image geladen wurde, wird die funktion ausgeführt*/
         img.onload = function () {
             imageArray.push(this);
-             // Create an <a-image> element for each loaded. Es wird ein neues HTML Element erstellt. Ein a-image Tag.
-             var aImage = document.createElement('a-image');
-             //Das erstellte a-image Tag bekommt noch Attribute/Eigenschaften. 
-             aImage.setAttribute('src', this.src);
-             aImage.setAttribute('position', '0 0 0'); // Set the desired position
- 
-             // Append the <a-image> to the <a-marker> Das a-image wird in den a-marker in HTML reingeladen. 
-             marker.appendChild(aImage);
+            // Create an <a-image> element for each loaded. Es wird ein neues HTML Element erstellt. Ein a-image Tag.
+            var aImage = document.createElement('a-image');
+            //Das erstellte a-image Tag bekommt noch Attribute/Eigenschaften. 
+            aImage.setAttribute('src', this.src);
+
+            //set random size (wight and height) for the image. Hier wird eine random Höhe und Breite erstellt. min ist 0.1 und max ist 0.5. Es wird die Formel von an Zeile 7 benutzt. Der random Wert der zurückgegeben wird, wird in der Variable randomSize gespeichert.
+            var randomSize = getRandomNumber(0.1, 0.5);
+            aImage.setAttribute('width', randomSize);
+            aImage.setAttribute('height', randomSize);
+
+            // Set random position for the image. 
+            var randomPositionX = getRandomNumber(-1, 1); // Adjust the range as needed
+            var randomPositionY = getRandomNumber(-1, 1); // Adjust the range as needed
+            var randomPositionZ = getRandomNumber(-1, 1);
+            aImage.setAttribute('position', `${randomPositionX} ${randomPositionY} ${randomPositionZ}`);
+
+            // Append the <a-image> to the <a-marker> Das a-image wird in den a-marker in HTML reingeladen. 
+            marker.appendChild(aImage);
         }
     }
 
